c690437df7f03bfa733bae2eb21702b2e6e2f0bf
kreker2112 <kreker2112@gmail.com>
2024-11-14 17:58:12 +0200

    some updates

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Orders from "../views/OrdersPage.vue";
 import Products from "../views/ProductsPage.vue";
 
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
