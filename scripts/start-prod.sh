d1f1e3698e6d6007a4904b169b269ce58a1046e4
kreker2112 <kreker2112@gmail.com>
2024-11-19 22:04:31 +0200

    some updates

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -7,9 +7,10 @@ cd "$(dirname "$0")/.."
 export SERVER_PORT=3000
 export WS_PORT=3001
 export CLIENT_PORT=8080
-export VUE_APP_API_URL=
-export VUE_APP_WS_URL=
-export VUE_APP_IMAGES_URL=/images
+export VUE_APP_API_URL=""  # Пустое значение
+export VUE_APP_WS_URL=""   # Пустое значение
+export VUE_APP_IMAGES_URL="/images"
+export ENVIRONMENT=production  # Добавляем ENVIRONMENT
 
 # Запускаем Docker Compose с использованием продакшен-конфигурации
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
