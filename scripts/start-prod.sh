ccad1577dc0d573d6503c5d68ad72001933b6d6b
Oleksandr Tkachov <kreker2112@gmail.com>
2024-11-21 04:08:52 +0200

    Update start-prod.sh

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Переходим в корневую директорию проекта
 cd "$(dirname "$0")/.."
 
-# Очищаем кеши Docker
 echo "Clearing Docker caches before starting production environment..."
 ./scripts/clear-docker-cache.sh
 
-# Устанавливаем переменные окружения для продакшена
 export SERVER_PORT=3000
 export WS_PORT=3001
 export CLIENT_PORT=8080
@@ -16,5 +13,4 @@ export VUE_APP_WS_URL=""
 export VUE_APP_IMAGES_URL=http://localhost:$CLIENT_PORT/images
 export ENVIRONMENT=production
 
-# Запускаем Docker Compose с использованием продакшен-конфигурации
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
