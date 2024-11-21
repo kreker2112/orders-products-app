de3fd4ee76aca0d3c75ab035e2993c21bd91d03a
Oleksandr Tkachov <kreker2112@gmail.com>
2024-11-21 04:10:30 +0200

    Update stop-prod.sh

--- scripts/stop-prod.sh
+++ scripts/stop-prod.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Переходим в корневую директорию проекта
 cd "$(dirname "$0")/.."
 
-# Очищаем кеши Docker
 echo "Clearing Docker caches before starting development environment..."
 ./scripts/clear-docker-cache.sh
 
-# Устанавливаем переменные окружения для продакшена
 export ENVIRONMENT=production
 export SERVER_PORT=3000
 export WS_PORT=3001
@@ -16,5 +13,4 @@ export VUE_APP_API_URL=""
 export VUE_APP_WS_URL=""
 export VUE_APP_IMAGES_URL=http://localhost:$CLIENT_PORT/images
 
-# Останавливаем и удаляем контейнеры
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
