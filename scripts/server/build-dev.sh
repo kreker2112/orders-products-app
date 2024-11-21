9064c6de2805fd3e9d65313fae66e55ca62446ab
Oleksandr Tkachov <kreker2112@gmail.com>
2024-11-21 04:11:27 +0200

    Update build-dev.sh

--- scripts/server/build-dev.sh
+++ scripts/server/build-dev.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Устанавливаем переменные для режима разработки
 ENVIRONMENT=development
 PORT=3000
 WS_PORT=3001
 IMAGE_NAME=orders-products-app-server-dev
 
-# Переходим в корневую директорию проекта
 cd "$(dirname "$0")/../.."
 
-# Сборка Docker-образа
 docker build \
   --build-arg ENVIRONMENT=$ENVIRONMENT \
   --build-arg PORT=$PORT \
@@ -18,7 +15,6 @@ docker build \
   -t $IMAGE_NAME \
   ./server
 
-# Проверка успешности сборки
 if [ $? -eq 0 ]; then
   echo "Docker image '$IMAGE_NAME' built successfully in development mode."
 else
