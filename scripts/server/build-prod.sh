55c72f1ff8f8c0e1725bcf3a71771856be19ad12
Oleksandr Tkachov <kreker2112@gmail.com>
2024-11-21 04:12:07 +0200

    Update build-prod.sh

--- scripts/server/build-prod.sh
+++ scripts/server/build-prod.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Устанавливаем переменные для режима продакшена
 ENVIRONMENT=production
 PORT=3000
 WS_PORT=3001
 IMAGE_NAME=orders-products-app-server-prod
 
-# Переходим в корневую директорию проекта
 cd "$(dirname "$0")/../.."
 
-# Сборка Docker-образа
 docker build \
   --build-arg ENVIRONMENT=$ENVIRONMENT \
   --build-arg PORT=$PORT \
@@ -18,7 +15,6 @@ docker build \
   -t $IMAGE_NAME \
   ./server
 
-# Проверка успешности сборки
 if [ $? -eq 0 ]; then
   echo "Docker image '$IMAGE_NAME' built successfully in production mode."
 else
