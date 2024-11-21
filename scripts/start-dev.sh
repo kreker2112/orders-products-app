6972649ba4c5744917fc6dd89dca5ca8bb188f05
Oleksandr Tkachov <kreker2112@gmail.com>
2024-11-21 04:08:10 +0200

    Update start-dev.sh

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Переходим в корневую директорию проекта
 cd "$(dirname "$0")/.."
 
-# Очищаем кеши Docker
 echo "Clearing Docker caches before starting development environment..."
 ./scripts/clear-docker-cache.sh
 
-# Устанавливаем переменные окружения для разработки
 export ENVIRONMENT=development
 export SERVER_PORT=3000
 export WS_PORT=3001
@@ -16,5 +13,4 @@ export VUE_APP_API_URL=http://localhost:$SERVER_PORT
 export VUE_APP_WS_URL=http://localhost:$WS_PORT
 export VUE_APP_IMAGES_URL=http://localhost:$CLIENT_PORT/images
 
-# Запускаем Docker Compose в режиме разработки
 docker-compose up --build -d
